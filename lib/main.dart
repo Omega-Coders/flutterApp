2e38eee85390763624094365275746b98f5e6d0f
Anirudh Saladi <anirudhsaladi7@gmail.com>
2022-01-06 11:26:41 +0530

    done with home screen template

--- lib/main.dart
+++ lib/main.dart
@@ -48,21 +48,70 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: Image.asset(
+            "assets/logo.png",
+            fit: BoxFit.contain,
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          toolbarHeight: 100.0, // double
+          automaticallyImplyLeading: false,
         ),
         body: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Stack(
+                fit: StackFit.loose,
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Container(
+                    height: 600,
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.8),
+                            spreadRadius: 3,
+                            blurRadius: 3,
+                            offset:
+                                Offset(2, 0.5), // changes position of shadow
+                          ),
+                        ],
+                        color: Colors.amber[600],
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        )),
+                  ),
+                  Container(
+                    height: 400,
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.8),
+                            spreadRadius: 3,
+                            blurRadius: 3,
+                            offset: const Offset(
+                                2, 0.5), // changes position of shadow
+                          ),
+                        ],
+                        color: Colors.blue[100],
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        )),
+                  )
+                ],
+              ),
               Center(
                 child: ElevatedButton(
                   onPressed: getImage,
                   child: Text('Scan'),
                 ),
               ),
-              SizedBox(height: 20),
-              Text('Cropped image path:'),
+              const SizedBox(height: 20),
+              const Text('Cropped image path:'),
               Padding(
                 padding: const EdgeInsets.only(top: 0, left: 0, right: 0),
                 child: Text(
@@ -74,7 +123,7 @@ class _MyAppState extends State<MyApp> {
               Visibility(
                 visible: _imagePath != null,
                 child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.all(16.0),
                   child: Image.file(
                     File(_imagePath ?? ''),
                   ),
